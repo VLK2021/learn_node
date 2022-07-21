1e8f27e056fdf44456c27ab7c70a97482a233e60
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-07-21 15:38:02 +0300

    hw2

--- homework2/app.js
+++ homework2/app.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const path = require('path');
+const {engine} = require('express-handlebars');
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'component')));
+app.set('view engine', '.hbs');
+app.engine('.hbs', engine({defaultLayout: false}));
+app.set('views', path.join(__dirname, 'component'));
+
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.use((req, res) => {
+    res.render('notFoundPage')
+})
+
 
 
 
