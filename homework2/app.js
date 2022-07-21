a082f8253a25fe9f18c20ba11c226281d4150111
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-07-21 16:54:50 +0300

    hw2

--- homework2/app.js
+++ homework2/app.js
@@ -4,10 +4,11 @@ const {engine} = require('express-handlebars');
 
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use(express.static(path.join(__dirname, 'component')));
+
 app.set('view engine', '.hbs');
 app.engine('.hbs', engine({defaultLayout: false}));
 app.set('views', path.join(__dirname, 'component'));
