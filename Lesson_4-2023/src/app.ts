e7f3fd97eb4f4e05ebb9a7f5313801452dba5ad3
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-04-04 15:06:44 +0300

    Lesson_4-2023 migration

--- Lesson_4-2023/src/app.ts
+++ Lesson_4-2023/src/app.ts
@@ -5,5 +5,5 @@ const app = express();
 console.log(users);
 
 app.listen(5500, () => {
-    console.log('Server has started!!!');
+    console.log('Server has started!');
 });
---
+++ Lesson_4-2023/src/migration/1680609910592-CreateTableUsers.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateTableUsers1680609910592 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
