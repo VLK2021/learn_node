67ce698e53010c2e5631395667f73cbdc5321d87
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-04-11 01:20:05 +0300

    del

--- Lesson_4-2023/src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { users } from './users';
-
-const app = express();
-console.log(users);
-
-app.listen(5500, () => {
-    console.log('Server has started!');
-});
